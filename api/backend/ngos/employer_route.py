4dd8df8bca5d053c58830db3ad7e298c8d282daf
Maria Alander <mariaalander@Marias-Macbook-Air.local>
2026-04-15 23:37:43 -0400

    Add employer route

---
+++ api/backend/ngos/employer_route.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, jsonify, request, current_app
+from backend.db_connection import get_db
+from mysql.connector import Error
+
+
+employer = Blueprint("employer", __name__)
+
+# Retrieve list of high potential students based on GPA/Major criteria [Jackson - 5]
+@employer.route("/students", methods=["GET"])
+def get_high_potential_students():
+    cursor = get_db().cursor(dictionary=True)
+    try:
+        gpa_cutoff = request.args.get("gpa", 3.5)
+        query = "SELECT * FROM STUDENT WHERE gpa >= %s"
+        cursor.execute(query, (gpa_cutoff,))
+        return jsonify(cursor.fetchall()), 200
+    finally:
+        cursor.close()
+
+# Return all detail information for a particular student. [Jackson - 1]
+@employer.route("/students/<int:id>", methods=["GET"])
+def get_student_detail(id):
+    cursor = get_db().cursor(dictionary=True)
+    try:
+        cursor.execute("SELECT * FROM STUDENT WHERE studentId = %s", (id,))
+        student = cursor.fetchone()
+        return jsonify(student) if student else (jsonify({"error": "Student not found"}), 404)
+    finally:
+        cursor.close()
+
+
+# List students who previously completed co-ops at the company. [Jackson - 6]
+@employer.route("/students/history", methods=["GET"])
+def get_coop_history(id):
+    cursor = get_db().cursor(dictionary=True)
+    try:
+        cursor.execute(
+          """SELECT u.firstName, u.lastName, s.email, s.major, s.GPA
+              FROM USER u
+              JOIN STUDENT s ON u.userId = s.userId
+              JOIN COOPEXPERIENCE ce ON s.studentId = ce.studentId
+              WHERE companyId = %s""",(id))
+        return jsonify(cursor.fetchall()), 200
+    finally:
+        cursor.close()
+
