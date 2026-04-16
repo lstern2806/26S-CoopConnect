e16b70d0e0cbb09455e6933366b6f3555ef95c60
EJD <ejdgm123@gmail.com>
2026-04-16 19:04:15 -0400

    Change to App description

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -9,7 +9,7 @@ st.write("# About this App")
 
 st.markdown(
     """
-    This is a demo app for Data and Software in International Government and Politics Dialogue 2025 Project Course.  
+    This is a demo app for CoopConnect.  
 
     The goal of this demo is to provide information on the tech stack 
     being used as well as demo some of the features of the various platforms. 
